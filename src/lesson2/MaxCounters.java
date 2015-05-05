78af53dab3122a80c09862b877635cb33b5c0e7a
Thirumalesh Chinna <tmhs.bits@gmail.com>
2015-05-05 18:21:03 +0530

    added latest code

--- src/lesson2/MaxCounters.java
+++ src/lesson2/MaxCounters.java
@@ -1,6 +1,5 @@
 package lesson2;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MaxCounters {
--- src/sorting/BubleSort.java
+++ src/sorting/BubleSort.java
@@ -4,13 +4,26 @@ public class BubleSort {
 
 	public static void main(String[] args) {
 		int[] A = { 3, 7, 4, 9, 5, 2, 6, 1 };
+		long startTime = System.nanoTime();
 		int[] B = solution(A);
+		long endTime = System.nanoTime();
 		for (int i : B)
 			System.out.println(i + ",");
+		System.out.println("time taken :"+(endTime - startTime));
 	}
 
 	public static int[] solution(int[] A) {
-		for
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 1; j < A.length - i; j++) {
+				if (A[j - 1] > A[j]) {
+					int temp = A[j];
+					A[j] = A[j - 1];
+					A[j - 1] = temp;
+				}
+			}
+
+		}
+		return A;
 	}
 
 }
